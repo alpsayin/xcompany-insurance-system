c59682cc70f08c54300dd04dca50a0f92669c41e
mertkaradogan <mertkaradogan@13ffe84b-d864-c845-e98a-f362c4c8bf52>
2011-10-16 00:56:48 +0000

    In DatabaseControl userList converted from ArrayList to HashMap. And some functions are filled.

--- xCompanyInsuranceManagementSystem/src/xcompany/control/DatabaseControl.java
+++ xCompanyInsuranceManagementSystem/src/xcompany/control/DatabaseControl.java
@@ -2,10 +2,19 @@ package xcompany.control;
 
 //  @ File Name : DatabaseControl.java
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import xcompany.structures.User;
+import xcompany.structures.Customer;
 import xcompany.structures.Garage;
 import xcompany.structures.Claim;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 
 //  @ Date : 11.10.2011
 
@@ -13,27 +22,87 @@ import java.util.ArrayList;
 
 
 public class DatabaseControl {
-	private ArrayList<Claim> getAllClaims() {
+
+    String usersFile = "AllUsers.bin";
+
+    /*
+     ^Because we dont have sign up (for now), we can use this function to create some users just once.
+     *
+     */
+    public boolean createSomeUsers() {
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream(usersFile);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            HashMap<String, User> userList = new HashMap<String, User>();
+            User customer1 = new Customer("mert", "karadogan", "gmertk", "gmertk@gmail.com", "12345",
+                    "Hanstavagen 49 Stockholm Sweden", 1);
+            User customer2 = new Customer("alp", "sayin", "alpsayin", "alp@gmail.com", "12345",
+                    "Akalla Stockholm Sweden", 2);
+            userList.put(Integer.toString(customer1.getId()), customer1);
+            userList.put(Integer.toString(customer2.getId()), customer2);
+
+            objectOutputStream.writeObject(userList);
+
+            objectOutputStream.close();
+            fileOutputStream.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        return true;
+    }
+
+    private ArrayList<Claim> getAllClaims() {
             ArrayList<Claim> claimList = new ArrayList<Claim>();
 
             return claimList;
-	}
-	
-	private ArrayList<User> getAllUsers() {
-            ArrayList<User> userList = new ArrayList<User>();
+    }
 
-            return userList;
-	}
-	
-	private ArrayList<Garage> getAllGarages() {
-            ArrayList<Garage> garageList = new ArrayList<Garage>();
+    private HashMap<String, User> getAllUsers(){
+        HashMap<String, User> userList = null;
 
-            return garageList;
-	}
-	
-	public ArrayList<Claim> getUserClaims(Object User) {
-            ArrayList<Claim> claimList = new ArrayList<Claim>();
+        try{
+            FileInputStream fileInputStream = new FileInputStream(usersFile);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 
-            return claimList;
-	}
+            userList = (HashMap<String, User>) objectInputStream.readObject();
+
+            objectInputStream.close();
+            fileInputStream.close();
+            
+        }
+        catch(Exception e){
+            System.err.println(e.toString());
+        }
+
+        return userList;
+
+    }
+
+    private ArrayList<Garage> getAllGarages() {
+        ArrayList<Garage> garageList = new ArrayList<Garage>();
+
+        return garageList;
+    }
+
+
+    public User getUser(int id){
+        String idString = id + "";
+
+        HashMap<String, User> userList = getAllUsers();
+        User user = userList.get(idString);
+
+        return user;
+    }
+
+
+    public ArrayList<Claim> getUserClaims(Object User) {
+        ArrayList<Claim> claimList = new ArrayList<Claim>();
+
+        return claimList;
+    }
+
 }
